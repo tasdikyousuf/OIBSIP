f28fe8669f860d4d90a1a559396ff5d018ebc85d
tasdikyousuf <mtasdikyousuf@gmail.com>
2023-12-11 15:32:15 +0600

    Create bmi_calculator.py

---
+++ bmi_calculator.py
@@ -0,0 +1,23 @@
+def bmi_calculator(weight, height):
+
+    bmi = weight / (height ** 2)
+    print(round(bmi,2))
+
+    if (bmi > 0):
+        if (bmi <= 18.5):
+            print("Underweight")
+        elif (bmi <= 25):
+            print("Normal")
+        elif (bmi <= 30):
+            print("Overweight")
+        elif (bmi <= 35):
+            print("Obese")
+        else:
+            print("Morbidly Obese")
+    else:
+        print("enter valid details")
+
+user_weight = float(input("Enter body mass in kilograms: "))
+user_height = float(input("Enter height in meters: "))
+
+bmi_calculator(user_weight, user_height)
