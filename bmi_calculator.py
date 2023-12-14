b8bfd7bd84c3d2863d4076d5f96711e23c4a2944
Tasdik <153457819+tasdikyousuf@users.noreply.github.com>
2023-12-15 01:36:53 +0600

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,8 +1,27 @@
 def calculate_bmi(weight, height):
+    """
+    Calculate Body Mass Index (BMI) based on weight (in kilograms) and height (in meters).
+
+    Parameters:
+    - weight (float): Body mass in kilograms.
+    - height (float): Height in meters.
+
+    Returns:
+    - float: Calculated BMI.
+    """
     bmi = weight / (height ** 2)
     return bmi
 
 def get_bmi_category(bmi):
+    """
+    Determine BMI category based on the calculated BMI.
+
+    Parameters:
+    - bmi (float): Calculated BMI.
+
+    Returns:
+    - str: BMI category (e.g., "Underweight", "Normal", "Overweight", "Obese", "Morbidly Obese").
+    """
     if bmi <= 18.5:
         return "Underweight"
     elif bmi <= 25:
@@ -15,6 +34,12 @@ def get_bmi_category(bmi):
         return "Morbidly Obese"
 
 def bmi_calculator():
+    """
+    Collect user input for weight and height, calculate BMI, and display the BMI category.
+
+    Returns:
+    - None: Prints the calculated BMI and BMI category.
+    """
     weight = float(input("Enter body mass in kilograms: "))
     height = float(input("Enter height in meters: "))
 
