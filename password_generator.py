ec49b3820feb473134b23a7f4273f36e37793f37
Tasdik <153457819+tasdikyousuf@users.noreply.github.com>
2023-12-15 01:35:28 +0600

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,6 +2,18 @@ import secrets
 import string
 
 def password_generator(length, digit, special):
+    """
+    Generates a password with specified length and optional inclusion of digits and special characters.
+
+    Parameters:
+    - length (int): Total length of the password.
+    - digit (str): 'y' if including digits, otherwise empty string.
+    - special (str): 'y' if including special characters, otherwise empty string.
+
+    Returns:
+    - None: Prints the generated password.
+    """
+
     pass_letters = string.ascii_letters
     pass_digits = string.digits
     pass_special = string.punctuation
@@ -10,28 +22,22 @@ def password_generator(length, digit, special):
 
     if digit == "y" and special == "y":
         pass_characters = pass_letters + pass_digits + pass_special
-        for i in range(length):
-            password += secrets.choice(pass_characters)
-        print(password)
-
     elif digit == "y" and special != "y":
         pass_characters = pass_letters + pass_digits
-        for i in range(length):
-            password += secrets.choice(pass_characters)
-        print(password)
-
     elif special == "y" and digit != "y":
         pass_characters = pass_letters + pass_special
-        for i in range(length):
-            password += secrets.choice(pass_characters)
-        print(password)
+    else:
+        print("Warning: You must select letters and at least one more criterion to generate a password.")
+        return
 
+    for i in range(length):
+        password += secrets.choice(pass_characters)
+
+    print(password)
 
 pass_length = int(input("Enter the total password length you want: "))
-
 print("Warning: You must select letters and at least one more criterion to generate a password.")
-
 want_digit = input("Press 'y' if you want numbers or press enter: ")
 want_special = input("Press 'y' if you want special characters or press enter: ")
 
-password_generator(pass_length, want_digit, want_special)
+password_generator(pass_length, want_digit, want_special) 
